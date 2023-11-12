063607ec80d43cd4a667a564494b671f346d8079
Micah Ives <micbenives@gmail.com>
2023-11-11 21:55:48 -0600

    Add additional modules, generateSvg function created

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// import { Shape, Circle } from './examples/lib/shapes.js';
+
+// for managing/altering file paths with path module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // App initializer function with inquirer prompts
 function init() {
@@ -27,14 +34,30 @@ function init() {
         },       
     ])
     .then((response) => {
-        writeSvg(response);
+        writeToFile(response);
     })
 }
 
-// Function to convert user responses into content for a generated .svg file
-function writeSvg(response) {
+// Function to create .svg file
+function writeToFile(response) {
+    const generateLogo = generateSvg(response)
 
-    fs.writeFile('logo.svg');
+    fs.writeFile(path.join(__dirname, '/examples', 'logo.svg'), generateLogo, (err) =>
+    err ? console.log(err) : console.log('Successfully created logo.svg!')
+)};
+
+function generateSvg(response) {
+    return `<svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    version="1.1" 
+    width="300" 
+    height="200">
+
+    <circle cx="150" cy="100" r="80" fill="green"/>
+
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+    </svg>`;
 }
 
 init();
