5f943df9cab610556910de66ef1c3eefa2ed5f87
Micah Ives <micbenives@gmail.com>
2023-11-13 14:26:48 -0600

    Add npm inquirer-maxlength-input-prompt

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Shape, Circle, Triangle, Square } from './lib/shapes.js';
-import { Recoverable } from 'repl';
+import MaxLengthInputPrompt from 'inquirer-maxlength-input-prompt';
+
+// register the prompt for the max-length capabilities for the first user prompt (up to 3 chars)
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+
 
 // for managing/altering file paths with path module
 const __filename = fileURLToPath(import.meta.url);
@@ -13,9 +17,10 @@ const __dirname = path.dirname(__filename);
 function init() {
     inquirer.prompt([
         {
-        type: 'input',
+        type: 'maxlength-input',
         name: 'text',
-        message: 'Enter up to three characters for your logo'
+        message: 'Enter up to three characters for your logo',
+        maxLength: 3
         },
         {
         type: 'input',
